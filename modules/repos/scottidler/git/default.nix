b8d591de9b447bdd7ce2670b79e150514a8ce649
Scott A. Idler <scott.a.idler@gmail.com>
2024-04-14 15:55:15 -0700

    got clone working as a separate script with a log file at ~/.clone/clone.log

--- modules/repos/scottidler/git/default.nix
+++ modules/repos/scottidler/git/default.nix
@@ -1,4 +1,32 @@
-{...}:
-{
+{ config, pkgs, lib, ... }:
 
+let
+  USER = config.home.username;
+  HOME = config.home.homeDirectory;
+  path = pkgs.fetchFromGitHub {
+    owner = "scottidler";
+    repo = "git";
+    rev = "055de809a5939b38780f5e596759254d78b30422";
+    sha256 = "sha256-E6Z1LgeDG3EJl408tPC305xRBa55o2VhcP+QkOU8a9Q=";
+  };
+in
+{
+  home.activation.createLogDir = lib.hm.dag.entryAfter ["writeBoundary"] ''
+    mkdir -p ${HOME}/.clone
+    chown ${config.home.username} ${HOME}/.clone
+    chmod 755 ${HOME}/.clone
+  '';
+  home.file = {
+    "${HOME}/bin/clone".source = "${path}/bin/clone";
+    "${HOME}/bin/clone-lite".source = "${path}/bin/clone-lite";
+    "${HOME}/bin/reponame".source = "${path}/bin/reponame";
+    "${HOME}/bin/git-objects".source = "${path}/bin/git-objects";
+    "${HOME}/bin/ls-git-repos".source = "${path}/bin/ls-git-repos";
+    "${HOME}/bin/ls-github-repos".source = "${path}/bin/ls-github-repos";
+    "${HOME}/bin/default-branch".source = "${path}/bin/default-branch";
+    "${HOME}/bin/stale-branches".source = "${path}/bin/stale-branches";
+    "${HOME}/bin/filter-ref".source = "${path}/bin/filter-ref";
+    "${HOME}/.shell-functions.d/clone.sh".source = "${path}/shell-functions.sh";
+    "${HOME}/.clone/clone/clone.cfg".source = "${path}/cfg/clone.cfg";
+  };
 }
--- repos.nix
+++ repos.nix
@@ -1,43 +1,7 @@
 { config, pkgs, lib }:
 let
-  #nvim = pkgs.fetchFromGitHub {
-  #  owner = "scottidler";
-  #  repo = "nvim";
-  #  rev = "b9f67dbff267faf8cf0757bcff9cc9305865c431";
-  #  sha256 = "sha256-Fg/g+CdnO7aIPCJcOBxEYUinwh2RjeWgf8xmyw3qvgo=";
-  #};
-
   HOME = config.home.homeDirectory;
   repos = [
-    #{
-    #  owner = "scottidler";
-    #  repo = "nvim";
-    #  rev = "9023255952d562bd3214e1fbbbf7c8233d85e4ee";
-    #  sha256 = "sha256-ZZyz04vsxonr2Sob8Gp6qfdDMUfu2mPhYKdoeod1TPM=";
-    #  links = {
-    #    "./" = "${HOME}/.config/nvim";
-    #  };
-    #}
-    {
-      owner = "scottidler";
-      repo = "helpful";
-      rev = "fdb84318c3ca560a7e858459649dfb9a5c08d49e";
-      sha256 = "sha256-gTn4Im8ydpQ06RvLD3KF1cX3qFQEYBqhmV/aUyPqb+0=";
-      links = {
-        "bin/cidr" = "${HOME}/bin/cidr";
-        "bin/say" = "${HOME}/bin/say";
-        "bin/speedtest" = "${HOME}/bin/speedtest";
-        "bin/tab" = "${HOME}/bin/tab";
-        "bin/tab1" = "${HOME}/bin/tab1";
-        "bin/tab2" = "${HOME}/bin/tab2";
-        "bin/tab3" = "${HOME}/bin/tab3";
-        "bin/tab4" = "${HOME}/bin/tab4";
-        "bin/toggle" = "${HOME}/bin/toggle";
-        "bin/prepend" = "${HOME}/bin/prepend";
-        "bin/toggle-remote-url" = "${HOME}/bin/toggle-remote-url";
-        "shell-functions.sh" = "${HOME}/.shell-functions.d/helpful.sh";
-      };
-    }
     {
       owner = "scottidler";
       repo = "cert-tools";
@@ -50,34 +14,6 @@ let
         "ssl-inspect" = "${HOME}/bin/ssl-inspect";
         "ssl-sans" = "${HOME}/bin/ssl-sans";
         "ssl-validity" = "${HOME}/bin/ssl-validity";
-      };
-    }
-    {
-      owner = "scottidler";
-      repo = "git";
-      rev = "a3f5bd07197037e5af2315770136ccd32f6424d4";
-      sha256 = "sha256-mxVJf918VE9hpkhhvfy8MDJICCevRdJWplmjfYI1PV8=";
-      links = {
-        "bin/clone" = "${HOME}/bin/clone";
-        "bin/clone-lite" = "${HOME}/bin/clone-lite";
-        "bin/reponame" = "${HOME}/bin/reponame";
-        "bin/git-objects" = "${HOME}/bin/git-objects";
-        "bin/ls-git-repos" = "${HOME}/bin/ls-git-repos";
-        "bin/ls-github-repos" = "${HOME}/bin/ls-github-repos";
-        "bin/default-branch" = "${HOME}/bin/default-branch";
-        "bin/stale-branches" = "${HOME}/bin/stale-branches";
-        "bin/filter-ref" = "${HOME}/bin/filter-ref";
-        "shell-functions.sh" = "${HOME}/.shell-functions.d/clone.sh";
-        "cfg/clone.cfg" = "${HOME}/.clone/clone/clone.cfg";
-      };
-    }
-    {
-      owner = "scottidler";
-      repo = "tmp";
-      rev = "03850b057066f525a827405ec6316a528b25ae91";
-      sha256 = "sha256-2sEJEVvUaZsb1InvUQ6DONYgG68WFm2eraqfWBRhWsM=";
-      links = {
-        "bin/tmp" = "${HOME}/bin/tmp";
       };
     }
     {
@@ -125,52 +61,8 @@ let
         "git-secrets" = "${HOME}/bin/git-secrets";
       };
     }
-    #{
-    # owner = "scottidler";
-    # repo = "pimp";
-    # rev = "specify-revision";
-    # sha256 = "specify-sha256";
-    # links = {
-    #   "pimp" = "${HOME}/bin/pimp";
-    # };
-    #}
-    #{
-    #  owner = "scottidler";
-    #  repo = "ls-stat";
-    #  rev = "specify-revision";
-    #  sha256 = "specify-sha256";
-    #  links = {
-    #    "ls-stat" = "${HOME}/bin/ls-stat";
-    #  };
-    #}
-    #{
-    #  owner = "scottidler";
-    #  repo = "y2j";
-    #  rev = "5fd3ad8dd5fb5a49ebd62f03e9b47699e00dd12c";
-    #  sha256 = "sha256-km8z/ozD3g4hhGGeqolS2peLdKmOfO92IF5qLiogSbY=";
-    #  links = {
-    #    "bin/y2j" = "${HOME}/bin/y2j";
-    #    "bin/j2y" = "${HOME}/bin/j2y";
-    #  };
-    #}
-    #{
-    #  owner = "tfutils";
-    #  repo = "tfenv";
-    #  rev = "specify-revision";
-    #  sha256 = "specify-sha256";
-    #  links = {
-    #    "bin/terraform" = "${HOME}/bin/terraform";
-    #  };
-    #}
   ];
 
-#in {
-#  mappings = {
-#    ".config/nvim".source = nvim;
-#  };
-#}
-
-  # Function to apply all repositories and mappings
   applyMappings = repo: let
     path = pkgs.fetchFromGitHub {
       inherit (repo) owner repo rev sha256;
@@ -180,6 +72,4 @@ let
 
 in {
   home.file = lib.foldl' (acc: repo: acc // applyMappings repo) {} repos;
-
-
 }
