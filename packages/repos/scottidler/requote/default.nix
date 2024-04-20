0f75ef0759eddfdf2217212dfcb6137dc5f9cd97
Scott A. Idler <scott.a.idler@gmail.com>
2024-04-20 16:58:52 -0700

    bumped to v0.1.5

--- packages/repos/scottidler/requote/default.nix
+++ packages/repos/scottidler/requote/default.nix
@@ -3,13 +3,13 @@
 { stdenv, fetchurl, autoPatchelfHook, gcc, glibc, lib, libgcc, ... }:
 
 let
-  version = "0.1.4";
+  version = "0.1.5";
   owner = "scottidler";
   repo = "requote";
   suffix = "linux";
   tarball = fetchurl {
     url = "https://github.com/${owner}/${repo}/releases/download/v${version}/requote-v${version}-${suffix}.tar.gz";
-    sha256 = "0495c72na8njy8gx6y43nzsv16532k7hfaf3r9g46ycjd7rkic2h";
+    sha256 = "0nzb694fn8k403b7v92xav8wl3qxnby5412wxfckmsbk0lqrhi86";
   };
 in stdenv.mkDerivation rec {
   pname = "requote";
