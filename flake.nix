ba799da66153762c9a5910fe168285ce3bbd8b17
Scott A. Idler <scott.a.idler@gmail.com>
2024-04-24 20:14:19 -0700

    ignoring flake.lock for now

--- flake.nix
+++ flake.nix
@@ -1,39 +1,35 @@
+# flake.nix
+
 {
-  description = "System configuration with NixOS and Home Manager";
+  description = "A comprehensive NixOS and Home Manager configuration for a single system";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
     home-manager.url = "github:nix-community/home-manager/release-23.11";
-    flake-utils.url = "github:numtide/flake-utils";
-    poetry2nix.url = "github:nix-community/poetry2nix";
   };
 
-  outputs = { self, nixpkgs, home-manager, flake-utils, poetry2nix, ... }:
-    flake-utils.lib.eachSystem [ "x86_64-linux" ] (system:
-      let
-        overlays = [ poetry2nix.overlay ];
-        pkgs = import nixpkgs {
-          inherit system overlays;
-        };
-        hmConfig = home-manager.lib.homeManagerConfiguration {
-          inherit pkgs system;
-          username = "saidler";
-          homeDirectory = "/home/saidler";
-          configuration = { config, lib, ... }:
-            import ./home.nix {
-              inherit config pkgs lib;
-            };
-        };
-      in {
-        nixosConfigurations = {
-          scottidler = nixpkgs.lib.nixosSystem {
-            inherit system pkgs;
-            modules = [ ./configuration.nix ];
-          };
-        };
-        homeManagerConfigurations = {
-          saidler = hmConfig;
-        };
-      });
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        overlays = [];
+      };
+      hmConfig = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs system;
+        username = "saidler";
+        homeDirectory = "/home/saidler";
+        configuration = import ./home.nix { inherit pkgs system; };
+      };
+    in {
+      nixosConfigurations.scottidler = nixpkgs.lib.nixosSystem {
+        inherit system pkgs;
+        modules = [
+          #/home/saidler/.config/home-manager/hardware-configuration.nix
+          ./hardware-configuration.nix
+          ./configuration.nix
+          hmConfig.home-manager.configuration
+        ];
+      };
+    };
 }
-
